91e3c6bfc80349c6874617f4c298da2254ed7b59
manimovassagh <mani.mgh@gmail.com>
2022-06-26 17:24:23 +0200

    done

--- index.js
+++ index.js
@@ -22,5 +22,36 @@ addEventToObject(animeImage2);
 addEventToObject(animeImage3);
 addEventToObject(animeImage4);
 
+// Make page to scroll smooth to Soecific Part
+
+// Part Fachrichtung
+const fachrichtungTeil = document.querySelector("#fachrichtungTeil");
+
+function gotToSection() {
+  fachrichtungTeil.scrollIntoView({ behavior: "smooth" });
+}
+const fachRichtungButton = document.querySelector(".FACHRICHTUNGEN");
+
+fachRichtungButton.addEventListener("click", gotToSection);
+
+// Part Kontakt
 
 
+const contactTeil=document.querySelector('#kontaktSection')
+
+function gotToKontakt() {
+  contactTeil.scrollIntoView({ behavior: "smooth" });
+}
+const kontaktButton=document.querySelector("body > div.container > div.menuPart > div.menuBrownPart > a.href.KONTAKT");
+
+kontaktButton.addEventListener("click",gotToKontakt)
+
+// Part EtagePlan
+const etageteil = document.querySelector('#etageplan')
+
+function gotToEtagePlan() {
+  etageteil.scrollIntoView({ behavior: "smooth" });
+}
+const etageButton = document.querySelector("#etageLink");
+
+etageButton.addEventListener("click",gotToEtagePlan)
